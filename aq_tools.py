3d3251e096fab48f57844796e442bb87a8ee8317
Mason Wilde <masonwilde@ku.edu>
2017-11-09 12:31:17 -0600

    Add disjunction function and tests with gumption

--- aq_tools.py
+++ aq_tools.py
@@ -6,3 +6,12 @@ def combine_complex(complex1, complex2):
         if selector2 not in new_complex:
             new_complex.append(selector2)
     return new_complex
+
+def disjunction(star1, star2, maxstar):
+    new_star = []
+    for complex1 in star1:
+        for complex2 in star2:
+            new_star.append(combine_complex(complex1, complex2))
+            if len(new_star) >= maxstar:
+                return new_star
+    return new_star
--- aq_tools_test.py
+++ aq_tools_test.py
@@ -13,6 +13,28 @@ class TestAQTools(unittest.TestCase):
         complex2 = [["Temp", "low"]]
         new_complex = aq_tools.combine_complex(complex1, complex2)
         self.assertEqual(new_complex, [["Temp", "low"], ["Cough", "no"]])
+    def test_disjunction_single_complex_single_selector(self):
+        star1 = [[["Temp", "low"]]]
+        star2 = [[["Headache", "yes"]]]
+        new_star = aq_tools.disjunction(star1, star2, 10)
+        self.assertEqual(new_star, [[["Temp", "low"], ["Headache", "yes"]]])
+    def test_disjunction_single_complex_multi_selector(self):
+        star1 = [[["Temp", "low"], ["Cough", "no"]]]
+        star2 = [[["Headache", "yes"], ["Cough", "no"]]]
+        new_star = aq_tools.disjunction(star1, star2, 10)
+        self.assertEqual(new_star, [[["Temp", "low"], ["Cough", "no"], ["Headache", "yes"]]])
+    def test_disjunction_multi_complex_single_selector(self):
+        star1 = [[["Temp", "low"]], [["Cough", "no"]]]
+        star2 = [[["Headache", "yes"]], [["Cough", "no"]]]
+        new_star = aq_tools.disjunction(star1, star2, 10)
+        self.assertEqual(new_star, [[["Temp", "low"], ["Headache", "yes"]], [["Temp", "low"], ["Cough", "no"]],
+                                    [["Cough", "no"], ["Headache", "yes"]], [["Cough", "no"]]])
+    def test_disjunction_multi_complex_single_selector_limit_maxstar(self):
+        star1 = [[["Temp", "low"]], [["Cough", "no"]]]
+        star2 = [[["Headache", "yes"]], [["Cough", "no"]]]
+        new_star = aq_tools.disjunction(star1, star2, 3)
+        self.assertEqual(new_star, [[["Temp", "low"], ["Headache", "yes"]], [["Temp", "low"], ["Cough", "no"]],
+                                    [["Cough", "no"], ["Headache", "yes"]]])
 
 if __name__ == '__main__':
     unittest.main()
