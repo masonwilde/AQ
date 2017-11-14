79ecb9f3385d1c34456f989767dca1a2e19b3028
Mason Wilde <masonwilde@ku.edu>
2017-11-14 11:14:17 -0600

    Added ability to calculate available attribute values for negated rules

--- rule_tools.py
+++ rule_tools.py
@@ -6,3 +6,11 @@ def rule_from_complex(complex1, n_decision):
     for selector in complex1:
         new_conditions.append(Condition(attribute =selector[0], value=selector[1]))
     return Rule(conditions = new_conditions, decision=Decision(n_decision[0], n_decision[1]))
+
+def get_available_attribute_values(dataset, rule):
+    attribute_values = {}
+    for condition in rule.conditions:
+        if condition.attribute not in attribute_values:
+            attribute_values[condition.attribute] = dataset.attribute_value_ranges[condition.attribute]
+        attribute_values[condition.attribute] = list(set(attribute_values[condition.attribute]) - set([condition.value]))
+    return attribute_values
--- rule_tools_test.py
+++ rule_tools_test.py
@@ -1,5 +1,10 @@
 import unittest
 import rule_tools
+from lers_reader import Lers_Reader
+from ruleset import Rule, Condition, Decision
+
+reader = Lers_Reader("simple_consistent.d")
+mock_dataset = reader.read()
 
 class TestRuleTools(unittest.TestCase):
     def test_rule_from_complex_single_selector(self):
@@ -9,6 +14,11 @@ class TestRuleTools(unittest.TestCase):
     def test_rule_from_complex_multi_selector(self):
         result = rule_tools.rule_from_complex([["Temperature", "high"], ["Headache", "no"]], ["Flue", "no"]).to_string()
         self.assertEqual(result, "(Temperature, not high) & (Headache, not no) -> (Flue, no)")
+
+    def test_get_available_attribute_values(self):
+        rule = Rule(conditions=[Condition(attribute="Temperature", value="very_high")], decision=Decision(label="Flue", value="no"))
+        result = rule_tools.get_available_attribute_values(mock_dataset, rule)
+        self.assertEqual(result, {"Temperature":["high", "normal"]})
 
 if __name__ == '__main__':
     unittest.main()
